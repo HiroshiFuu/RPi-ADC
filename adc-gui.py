63b5554253e00c94950b3b73d47cac4eaf3fd1bf
Feng Hao <feng0060@e.ntu.edu.sg>
2016-01-27 13:31:43 +0800

    detail commented

--- adc-gui.py
+++ adc-gui.py
@@ -18,20 +18,11 @@ for i in range(4):
 	rb = f.read(2)
 	if len(rb) != 2:
 		continue
-	#unpack the binary stream, 'H' for unsigned short
 	res = struct.unpack('H', rb)
 	res = int(res[0])
 	buf[0] = res & 0x00FF
 	buf[1] = res >> 8
-	
-	#Frame 1 
-	#bit 6 - CH id 1
-	#bit 5 - CH id 0
-	#bit 2:1 - Data bit 10:9
 	channel = (buf[0] & 0b00110000) >> 4
-	#Frame 12
-	#bit 8:1 - Data bit 8:1
 	data = (float)((buf[0] & 0x0F) << 8) + buf[1]
-	#data = data / 4096;
-	print "Channel %02d Data: %.2f\n" % (channel + 1, data);
+	print "Channel %02d Data: %04f\n" % (channel + 1, data);
 	time.sleep(0.1)
